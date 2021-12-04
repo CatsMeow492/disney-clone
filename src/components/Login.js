4e516dbef327b79706342c2df93369c3712503f8
CatsMeow492 <calisurf243@hotmail.com>
2021-12-03 16:13:21 -0800

    added assets to public/images

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,6 +5,9 @@ const Login = (props) => {
     return (
     <Container>
         <Content>
+            <CTA>
+                <CTALogoOne src='/images/cta-logo-one.png' alt='' />
+            </CTA>
             <BgImage />
         </Content>
     </Container>
@@ -21,30 +24,42 @@ const Container = styled.section`
 `;
 
 const Content = styled.div`
-margin-bottom: 10vw;
-width: 100%;
-position: relative;
-min-height: 100vh;
-box-sizing: border-box;
-display: flex;
-justify-content: center;
-align-items: center;
-flex-direction: column;
-padding: 80px 40px;
-height: 100%;
+    margin-bottom: 10vw;
+    width: 100%;
+    position: relative;
+    min-height: 100vh;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 80px 40px;
+    height: 100%;
 `;
 
 const BgImage = styled.div`
-height: 100%;
-background-position: top;
-background-size: cover;
-background-repeat: no-repeat;
-background-image: url("/images/login-background.jpeg");
-position: absolute;
-top: 0;
-right: 0;
-left: 0;
-z-index: -1; 
+    height: 100%;
+    background-position: top;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-image: url("/images/login-background.jpeg");
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    z-index: -1; 
+`;
+
+const CTA = styled.div`
+
+`;
+
+const CTALogoOne = styled.img`
+margin-bottom: 12px;
+max-width: 600px;
+min-height: 1px;
+display: block;
+width: 100%;
 `;
 
 export default Login;
