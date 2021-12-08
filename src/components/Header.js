de50f623e820af71b97172f4ded04591e459dd7c
CatsMeow492 <calisurf243@hotmail.com>
2021-12-08 14:01:43 -0800

    header is stable but line transform effect needs to be fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,67 @@
 import styled from 'styled-components'
 
 const Header = (props) => {
-    return <Nav>
-        Header</Nav>
+    return ( 
+    
+    <Nav>
+        <Logo src='/images/logo.svg' alt=''></Logo>
+        <NavMenu>
+            <a href='/home'>
+                <img src='/images/home-icon.svg' alt='HOME'>
+                </img>
+            <span>
+                        HOME
+            </span>
+            </a>
+            <a href='/search'>
+                <img src='/images/search-icon.svg' alt='SEARCH'>
+                </img>
+            <span>
+                        SEARCH
+            </span>
+            </a>
+            <a href='/watchlist'>
+                <img src='/images/watchlist-icon.svg' alt='WATCHLIST'>
+                </img>
+            <span>
+                        WATCHLIST
+            </span>
+            </a>
+            <a href='/original'>
+                <img src='/images/original-icon.svg' alt='ORIGINAL'>
+                </img>
+            <span>
+                        ORIGINAL
+            </span>
+            </a>
+            <a href='/series'>
+                <img src='/images/series-icon.svg' alt='SERIES'>
+                </img>
+            <span>
+                        SERIES
+            </span>
+            </a>
+            <a href='/movies'>
+                <img src='/images/movie-icon.svg' alt='MOVIES'>
+                </img>
+            <span>
+                        Movies
+            </span>
+            </a>
+        </NavMenu>
+        <Login>Login</Login>
+    </Nav>
+    
+    )
 }
 
 const Nav = styled.nav`
-    position: fixed;
+    
     top: 0;
     left: 0;
     right: 0;
     height: 70px;
-    background-color: transparent;
+   
     display: flex;
     justify-content: flex-end;
     align-items: center;
@@ -20,6 +70,101 @@ const Nav = styled.nav`
     transition: opacity 0.5s ease-out; 
     letter-spacing: 16px;
     z-index: 3;
+`;
+
+const Logo = styled.img`
+    padding: 0;
+    width: 80px;
+    margin-top: 4;
+    max-height: 70px;
+    font-size: 0;
+    display: inline-block
+    img {
+        display: block;
+        width: 100%;
+    }
+`;
+
+const NavMenu = styled.div`
+    align-items: center;
+    display: flex;
+    flex-flow: row nowrap;
+    height: 100%;
+    justify-content: flex-end;
+    margin: 0px;
+    padding: 0px;
+    position: relative;
+    margin-right: auto;
+    margin-left: 25px;
+
+    a {
+        display: flex;
+        align-items: center;
+        padding: 0 12px;
+    }
+
+    img {
+        height: 20px;
+        min-width: 20px;
+        width: 20px;
+        z-index: auto;
+    }
+
+    span {
+        color: rgb(249, 249, 249);
+        font-size: 13px;
+        letter-spacing: 1.42px;
+        line-height: 1.08;
+        padding: 2px 0px;
+        white-space: nowrap;
+        position: relative;
+    
+        &:before {
+            content: "";
+            display: block;
+            background-color: rgb(249, 249, 249);
+            border-radius: 0px 0px 4px 4px;
+            bottom: -6px;
+            height: 2px;
+            left: 0;
+            opacity: 0;
+            position: absolute;
+            right: 0px;
+            transform-origin: left center;
+            transform: scaleX(0);
+            transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+            visibility: hidden;
+            width: auto;
+        }
+    }
+
+    &:hover {
+        span:before {
+            transform: scaleX(1);
+            visibility: visible;
+            opacity: 1 !important;
+        }
+    }
+
+    // @media (max-width: 768px) {
+    //     display: none;
+    // }
+`;
+
+const Login = styled.a`
+    background-color: rgb(0, 0, 0, 0);
+    border: 1px solid #f9f9f9;
+    border-radius: 4px;
+    padding: 8px 16px;
+    text-transform: uppercase;
+    letter-spacing: 1.5px;
+    transition: all .2s ease 0s;
+
+    &:hover {
+        background-color: #f9f9f9;
+        color: #000;
+        border-color: transparent;
+    }
 `;
 
 export default Header;
