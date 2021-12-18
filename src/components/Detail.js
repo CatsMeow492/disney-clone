0f50b59d258d30f0e63278a5d223298cd7ba33a7
CatsMeow492 <calisurf243@hotmail.com>
2021-12-18 14:24:20 -0800

    third commit - stable

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -2,20 +2,43 @@ import styled from 'styled-components';
 
 const Detail = (props) => {
     return (
-        <Container>
 
+        <Container>
+                <Background>
+
+                </Background>
         </Container>
-        
+
         )
 };
 
-export default Detail;
-
 const Container = styled.div`
+
     position: relative;
     min-height: calc(100vh- 250px);
     overflow-x: hidden;
     display: block;
     top: 72px;
-    padding: 0 calc(3.5vw - 5px)
+    padding: 0 calc(3.5vw - 5px);
+
 `;
+
+const Background = styled.div`
+
+    left: 0px;
+    opacity: 0.8;
+    position: fixed;
+    right: 0px;
+    top: 0px;
+    z-index: -1;
+
+    img{
+        width: 100vh;
+        height: 200vh;
+    }
+
+`;
+
+
+export default Detail;
+
